985c9990774b4e32cbbcfef13be3d46f2d4f7a7c
OwenLaRosa <owen.larosa@outlook.com>
2015-11-11 15:34:43 -0500

    refactor: Add Bullet class

---
+++ SpaceInvaders/Bullet.swift
@@ -0,0 +1,38 @@
+//
+//  Bullet.swift
+//  SpaceInvaders
+//
+//  Created by Owen LaRosa on 11/11/15.
+//  Copyright © 2015 Owen LaRosa. All rights reserved.
+//
+
+import SpriteKit
+
+class Bullet: SKSpriteNode {
+    
+    init(name: String) {
+        var bulletColor = SKColor()
+        if name == kPlayerBulletName {
+            bulletColor = kPlayerBulletColor
+        } else if name == kAlienBulletName {
+            bulletColor = kAlienBulletColor
+        }
+        super.init(texture: nil, color: bulletColor, size: kBulletSize)
+        self.name = name
+        
+        addPhysics()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// Add the basic physics properties for all bullets
+    func addPhysics() {
+        physicsBody = SKPhysicsBody(rectangleOfSize: kBulletSize)
+        physicsBody?.dynamic = true
+        physicsBody?.affectedByGravity = false
+        physicsBody?.allowsRotation = false
+    }
+    
+}
