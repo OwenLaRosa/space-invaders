3014feb37d2b606963db3d3e9ad53a93a9485eb4
OwenLaRosa <owen.larosa@outlook.com>
2015-11-13 17:18:28 -0500

    Add GameData class

---
+++ SpaceInvaders/GameData.swift
@@ -0,0 +1,23 @@
+//
+//  GameData.swift
+//  SpaceInvaders
+//
+//  Created by Owen LaRosa on 11/13/15.
+//  Copyright © 2015 Owen LaRosa. All rights reserved.
+//
+
+import Foundation
+
+class GameData {
+    
+    var level: Int
+    var score: Int
+    var lives: Int
+    
+    init(level: Int) {
+        self.level = level
+        score = 0
+        lives = 1
+    }
+    
+}
