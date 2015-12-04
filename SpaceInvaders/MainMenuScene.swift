4f38e3e6057e42e4f15ee6e3ba69457b1d69067b
OwenLaRosa <owen.larosa@outlook.com>
2015-12-04 15:10:34 -0500

    Comments MainMenuScene
    uncomment super() implementation in didMoveToView()

--- SpaceInvaders/MainMenuScene.swift
+++ SpaceInvaders/MainMenuScene.swift
@@ -13,7 +13,7 @@ class MainMenuScene: SKScene {
     let buttonPadding: CGFloat = 8
     
     override func didMoveToView(view: SKView) {
-        //super.didMoveToView(view)
+        super.didMoveToView(view)
         
         configureUI()
     }
@@ -40,6 +40,7 @@ class MainMenuScene: SKScene {
     }
     
     func addButtonsAndLabels(container: SKSpriteNode) {
+        // display the game's name in monospace font
         let titleLabel = SKLabelNode(text: "SPACE INVADERS")
         titleLabel.fontName = "Courier-Bold"
         titleLabel.fontColor = SKColor.blueColor()
@@ -48,6 +49,7 @@ class MainMenuScene: SKScene {
         titleLabel.position = CGPoint(x: 0, y: container.size.height/2 - titleLabel.frame.size.height)
         container.addChild(titleLabel)
         
+        // play button, launches the game at the starting level
         let playButton = ButtonNode()
         playButton.text = "Play"
         playButton.fontSize = 40
@@ -63,6 +65,7 @@ class MainMenuScene: SKScene {
         }
         addButton(toContainer: container, node: playButton)
         
+        // options button to present a view for game settings
         let optionsButton = ButtonNode()
         optionsButton.text = "Options"
         optionsButton.fontSize = 40
@@ -72,11 +75,13 @@ class MainMenuScene: SKScene {
         addButton(toContainer: container, node: optionsButton)
     }
     
+    /// Create a background and add the button to the scene.
     func addButton(toContainer container: SKNode, node: SKNode) {
         let buttonBackground = ButtonBackground(button: node as! ButtonNode)
         buttonBackground.size.width = 200
         buttonBackground.position = node.position
         
+        // add both to the container: background first and then the button
         container.addChild(buttonBackground)
         container.addChild(node)
     }
