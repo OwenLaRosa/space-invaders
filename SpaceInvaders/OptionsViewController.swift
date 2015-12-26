c4a5f325b5691df154f7e588d35226fad5bd2dc4
OwenLaRosa <owen.larosa@outlook.com>
2015-12-26 18:18:09 -0500

    Implement table view data source
    Also add prototype cell to storyboard

--- SpaceInvaders/OptionsViewController.swift
+++ SpaceInvaders/OptionsViewController.swift
@@ -15,3 +15,52 @@ class OptionsViewController: UIViewController {
     }
     
 }
+
+extension OptionsViewController: UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        switch section {
+        case 0:
+            rows = 1
+        default:
+            break
+        }
+        return rows
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            cell = tableView.dequeueReusableCellWithIdentifier("PickerViewCell")!
+        default:
+            break
+        }
+        configureCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        switch section {
+        case 0:
+            title = "Controls"
+        default:
+            break
+        }
+        return title
+    }
+    
+    private func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        default:
+            break
+        }
+    }
+    
+}
