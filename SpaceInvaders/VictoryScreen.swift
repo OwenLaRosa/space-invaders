96b36f4f2b08eb14eb1483343ade46cf12fb2a85
OwenLaRosa <owen.larosa@outlook.com>
2016-01-07 16:56:18 -0500

    Add buttons/labels to victory screen

--- SpaceInvaders/VictoryScreen.swift
+++ SpaceInvaders/VictoryScreen.swift
@@ -10,12 +10,15 @@ import SpriteKit
 
 class VictoryScreen: SKSpriteNode {
     
+    let buttonPadding: CGFloat = 8
+    
     init() {
         super.init(texture: nil, color: SKColor.blackColor(), size: CGSize(width: kUniversalScreenWidth, height: kUniversalScreenHeight))
         
         alpha = 0.8
         userInteractionEnabled = true
         
+        setupUI()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -23,7 +26,31 @@ class VictoryScreen: SKSpriteNode {
     }
     
     private func setupUI() {
+        let victoryLabel = SKLabelNode()
+        victoryLabel.text = "Well done, Earthling."
+        victoryLabel.fontName = "Courier-Bold"
+        victoryLabel.fontColor = SKColor.greenColor()
+        victoryLabel.verticalAlignmentMode = .Center
+        victoryLabel.position = CGPoint(x: 0, y: size.height/2 - victoryLabel.frame.size.height - buttonPadding)
+        addChild(victoryLabel)
         
+        let messageLabel = SKLabelNode()
+        messageLabel.text = "You win this time!"
+        messageLabel.fontName = "Courier"
+        messageLabel.verticalAlignmentMode = .Center
+        messageLabel.position = CGPoint(x: 0, y: victoryLabel.position.y - victoryLabel.frame.size.height)
+        addChild(messageLabel)
+        
+        let mainMenuButton = ButtonNode()
+        mainMenuButton.text = "Main Menu"
+        
+        mainMenuButton.verticalAlignmentMode = .Center
+        mainMenuButton.position = CGPoint(x: 0, y: 0)
+        mainMenuButton.callback = {
+            // go back to main menu
+            self.scene!.view!.presentScene(MainMenuScene(), transition: SKTransition.doorwayWithDuration(1.0))
+        }
+        addChild(mainMenuButton)
     }
     
 }
