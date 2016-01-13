ba8314a30c4094b8eb7e2b005423582f91f4c73a
OwenLaRosa <owen.larosa@outlook.com>
2016-01-12 19:28:58 -0500

    Add global setting for control size

--- SpaceInvaders/Settings.swift
+++ SpaceInvaders/Settings.swift
@@ -18,5 +18,15 @@ enum ControlScheme: Int {
     case RightSide
 }
 
+/// Sizes for control buttons
+enum ControlSize: Int {
+    case Small = 50
+    case Medium = 75
+    case Large = 100
+}
+
+/// The currently selected control size
+var currentControlSize: ControlSize = .Medium
+
 /// The currently selected control scheme.
 var currentControlScheme: ControlScheme = .BothSides
